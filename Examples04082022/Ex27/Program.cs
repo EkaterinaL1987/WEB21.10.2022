92a47d753fe9c3dc96c360edb0e390e7cc1ac6a5
Lyamina Ekaterina <katjamina@rambler.ru>
2022-08-04 20:03:25 +0300

    Задача 27 решена

--- Examples04082022/Ex27/Program.cs
+++ Examples04082022/Ex27/Program.cs
@@ -1,1 +1,10 @@
 ﻿//Напишите метод, которая принимает на вход число и выдаёт сумму цифр в числе.
+int a = int.Parse(Console.ReadLine());
+            int s = 0;
+            while (a > 0)
+            {
+                 s = s + a % 10;
+                a = a /10 ;
+             }
+ Console.WriteLine(s);
+ 
