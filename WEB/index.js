ee9c7209eddcbeea7baaa1e721a27efb1eeefac3
Lyamina Ekaterina <katjamina@rambler.ru>
2022-10-25 09:17:34 +0300

    Изучение JS

--- WEB/index.js
+++ WEB/index.js
@@ -3,5 +3,14 @@ alert("Hello,JS!");
 let result = 158+2;
 alert("Результат:"+ result+2)
 alert("Результат: ${158+2}")
+function test()
+{
+    var x = 1
+    if (tru)
+    {var x = 2
+        console.log(x)
+    }
+    console.log(x)
 
-
+}
+test()
